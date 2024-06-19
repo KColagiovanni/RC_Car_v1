2fee7dcdd2a04fc7508d1f422e9b3d8071c7dcaf
KColagi1 <kcolagi1@its.jnj.com>
2024-06-18 17:33:51 -0700

    Still working on lights functionality.

--- static/js/main.js
+++ static/js/main.js
@@ -34,25 +34,56 @@ function stop(){
 //        .then(data => console.log(data))
 //}
 
-function sequentialFlashing(){
-    var slow = document.getElementById("slowFlashSpeed");
-    var intermediate = document.getElementById("intermediateFlashSpeed");
-    var fast = document.getElementById("fastFlashSpeed");
-    var flashSpeed;
+function flashingLights(){
+//    var sequentialLights = document.getElementById("sequentialFlashingLights")
+//    var flashingPoliceLights = document.getElementById("flashingPoliceLights")
+//    var pairedFlashingLights = document.getElementById("pairedFlashingLights")
+//
+//    var slow = document.getElementById("slowFlashSpeed");
+//    var intermediate = document.getElementById("intermediateFlashSpeed");
+//    var fast = document.getElementById("fastFlashSpeed");
+//    var flashSpeed;
+//
+//    if (slow.checked == true){
+//        flashSpeed = 0.5
+//    } else if (intermediate.checked == true){
+//        flashSpeed = 0.25
+//    } else {
+//        flashSpeed = 0.1
+//    }
+//
+//    if (sequentialLights.checked == true){
+//        sequentialFlashing(flashingSpeed)
+//    } else if (flashingPoliceLights == true){
+//        policeLights(flashingSpeed)
+//    } else {
+//        pairedFlashing(flashingSpeed)
+//    }
+    fetch('/lights')
+        .then(response => response.json)
+        .then(data => console.log(data))
 
-    if (slow.checked == true){
-        flashSpeed = 0.5
-    } else if (intermediate.checked == true){
-        flashSpeed = 0.25
-    } else {
-        flashSpeed = 0.1
-    }
-    response = {'data': flashingSpeed}
+}
+
+function sequentialFlashing(flashSpeed){
+//    var slow = document.getElementById("slowFlashSpeed");
+//    var intermediate = document.getElementById("intermediateFlashSpeed");
+//    var fast = document.getElementById("fastFlashSpeed");
+//    var flashSpeed;
+
+//    if (slow.checked == true){
+//        flashSpeed = 0.5
+//    } else if (intermediate.checked == true){
+//        flashSpeed = 0.25
+//    } else {
+//        flashSpeed = 0.1
+//    }
+//    response = {'data': flashSpeed}
     fetch('/sequentialFlashing')
         .then(response => response.json)
         .then(data => console.log(data))
 
-    console.log("Sequential Flashing Lights")
+    console.log("Sequential Flashing Lights" + flashSpeed)
 }
 
 function policeLights(){
